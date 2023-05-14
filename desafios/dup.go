7b2d27f502b4c38faa7d903b2925f0913d0690cb
CelsonF <celson338540@gmail.com>
2023-05-14 10:40:48 -0300

    Challenger resolver with o^n

--- desafios/dup.go
+++ desafios/dup.go
@@ -4,5 +4,12 @@ package main
 Dado um array inteiro nums, retorne true se qualquer valor aparecer pelo menos duas vezes no array e retorne false se cada elemento for distinto.
 */
 func contemDuplicado(nums []int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				return true
+			}
+		}
+	}
 	return false
 }
